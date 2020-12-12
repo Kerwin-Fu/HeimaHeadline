edc86dc0b38ce22cfec368c62109902d71541f96
Kerwin <1078933746@qq.com>
2020-12-12 14:48:52 +0800

    登陆页面完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
@@ -10,5 +11,4 @@ export default {
 </script>
 
 <style lang="less">
-
 </style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="login-container">
+    <van-nav-bar class="page-nav-bar" title="标题" />
+    <van-form @submit="onSubmit" ref="loginForm">
+      <van-field
+        name="mobile"
+        placeholder="请输入手机号"
+        v-model="user.mobile"
+        clearable
+        :rules="userFormRules.mobile"
+        maxlength="11"
+        type="number"
+      >
+        <i slot="left-icon" class="iconfont icon-shouji"></i>
+      </van-field>
+      <van-field
+        name="code"
+        placeholder="请输入验证码"
+        v-model="user.code"
+        :rules="userFormRules.code"
+        maxlength="6"
+        type="number"
+      >
+        <i slot="left-icon" class="iconfont icon-yanzhengma"></i>
+        <template #button>
+          <van-count-down
+            v-if="isCountDownShow"
+            slot="button"
+            :time="1000 * 5"
+            format="ss s"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            v-else
+            class="send-sms-btn"
+            round
+            size="small"
+            type="default"
+            native-type="button"
+            @click="onSendSms"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button block type="info" native-type="submit"> 提交 </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import { login, getSmsCode } from '@/api/user.js'
+export default {
+  name: 'LoginPage',
+  components: {},
+  props: {},
+  data() {
+    return {
+      // 控制倒计时组件的开关
+      isCountDownShow: false,
+      user: {
+        mobile: '',
+        code: ''
+      },
+      userFormRules: {
+        mobile: [
+          {
+            required: true,
+            message: '手机号不能为空'
+          },
+          {
+            pattern: /^1[3|5|7|8]\d{9}$/,
+            message: '手机号格式错误'
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '验证码不能为空'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码格式错误'
+          }
+        ]
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    async onSubmit() {
+      const user = this.user
+      this.$toast.loading({
+        duration: 0, // 持续时间，0表示持续展示不停止
+        forbidClick: true, // 是否禁止背景点击
+        message: '登录中...' // 提示消息
+      })
+      try {
+        const res = await login(user)
+        console.log(res)
+        this.$store.commit('setUser', res.data.data)
+        this.$toast.success('登录成功')
+      } catch (err) {
+        if (err.response.status === 400) {
+          this.$toast.fail('手机号或者验证码错误')
+        } else {
+          this.$toast.fail('登录失败，请稍后重试')
+        }
+      }
+    },
+    async onSendSms() {
+      try {
+        await this.$refs.loginForm.validate('mobile')
+      } catch (err) {
+        return console.log('验证失败', err)
+      }
+      this.isCountDownShow = true
+      try {
+        await getSmsCode(this.user.mobile)
+        this.$toast('发送成功')
+      } catch (err) {
+        this.isCountDownShow = false // 发送失败，关闭倒计时
+        if (err.response.status === 429) {
+          this.$toast('发送太频繁了，请稍后重试')
+        } else {
+          this.$toast('发送失败，请稍后重试')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.login-container {
+  .iconfont {
+    font-size: 37px;
+  }
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
